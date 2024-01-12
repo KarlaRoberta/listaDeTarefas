3222d7ee8e39c48e2be3a39680325df8eec18b1c
KarlaR <karlarobertati@gmail.com>
2024-01-11 21:38:49 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+
+import './App.css'
+
+
+
+const App = () => {
+  return (
+    <div>
+
+<div className='header'>
+
+<div className='imagem_perfil'></div>
+
+<div className='descricao_perfil'>
+
+<h1>Pedro Souza</h1>
+<h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat quasi, nemo adipisci libero architecto praesentium nisi consequatur laborum reiciendis amet molestias numquam? Doloribus odit nemo perferendis reiciendis aliquid ipsum nobis!</h3>
+
+
+<div className='links'>
+  <ul>
+    <li><a href="#">Link 1</a></li>
+    <li><a href="#">Link 2</a></li>
+    <li><a href="#">Link 3</a></li>
+    <li><a href="#">Link 4</a></li>
+  </ul>
+</div>
+</div>
+
+<section>
+<h1>Lista de tarefas</h1>
+<h2>Dia</h2>
+<h2>Noite</h2>
+</section>
+
+</div>
+
+
+    </div>
+  )
+}
+
+export default App
+
